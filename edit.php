03b7f7d34ceaf61002d23cd2b9da4b32b758a470
Muhammad Ikram Daffa Ramdhani <64885373+IkramDaffa@users.noreply.github.com>
2020-12-12 23:58:26 +0700

    update

---
+++ edit.php
@@ -0,0 +1,9 @@
+<?php
+include "koneksi.php";
+$nim=$_POST["id"];
+$nama=$_POST["nama"];
+$prodi=$_POST["prodi"];
+$angkatan=$_POST["angkatan"];
+$sintaks="UPDATE mahasiswa set nama='$nama', prodi='$prodi', angkatan='$angkatan' where nim='$nim'";
+$urut=mysqli_query($koneksi, $sql);
+?>
